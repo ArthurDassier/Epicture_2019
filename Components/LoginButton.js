b893773516e87b4ad4272ccc0f6e44f86390b2c3
Arthur Dassier <arthur.dassier@epitech.eu>
2019-10-11 17:30:10 +0200

    Grosse merde

--- Components/LoginButton.js
+++ Components/LoginButton.js
@@ -1,27 +1,40 @@
 //import liraries
 import React, { Component } from 'react';
 import { View, Text, TextInput, TouchableOpacity, Alert, Button ,StyleSheet ,StatusBar} from 'react-native';
+import { authorize } from 'react-native-app-auth';
 
-const onButtonPress = () => {
-    fetch('https://api.imgur.com/oauth2/token', {
-        method: 'POST',
-        body: JSON.stringify({
-            "access_token": "ef90055611059f6006249821a176127c061fd97c",
-            "expires_in": 315360000,
-            "token_type": "bearer",
-            "scope": "",
-            "refresh_token": "5499a9a6f7683df0d67fe96d80eb7c3dea73da0b",
-            "account_id": 115081187,
-            "account_username": "arthurdassier"
-        }),
-      }).then((response) => response.json()).then((responseJson) => {
-        return responseJson;
-      })
+
+const config = {
+    issuer: 'https://api.imgur.com',
+    clientId: 'fc7cb1f5f86d0ef',
+    clientSecret: '3c08f63cbc54b6dae4b2ff05144e38ccc9a1aeb4',
+    redirectUrl: 'epicture.arthurdassier.com',
+    scopes: [],
+    serviceConfiguration: {
+        authorizationEndpoint: 'https://api.imgur.com/oauth2/authorize',
+        tokenEndpoint: 'https://api.imgur.com/oauth2/token'
+    }
+};
+
+const onButtonPress = async() => {
+    try {
+        console.log('-----');
+        const result = await authorize(config);
+        console.log('---+++++--');
+        // result includes accessToken, accessTokenExpirationDate and refreshToken
+    } catch (error) {
+        console.log(config.redirectUrl);
+        console.log(error);
+      }
 };
 
 
 // create a component
 class LoginButton extends Component {
+    constructor( props ) {
+        super( props )
+    }
+
     render() {
         return (
             <View style={styles.container}>
